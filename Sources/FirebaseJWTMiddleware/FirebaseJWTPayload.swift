9876ede42693d16411391e5de8da740f5c7040d4
Baris Atamer <brsatamer@gmail.com>
2020-04-05 18:27:39 +0200

    feat(firebaseJwtPayload): update documentation

--- Sources/FirebaseJWTMiddleware/FirebaseJWTPayload.swift
+++ Sources/FirebaseJWTMiddleware/FirebaseJWTPayload.swift
@@ -23,23 +23,30 @@ public struct FirebaseJWTPayload: JWTPayload {
         case phoneNumber = "phone_number"
     }
     
+    /// Issuer. It must be "https://securetoken.google.com/<projectId>", where <projectId> is the same project ID used for aud
     public let issuer: IssuerClaim
-    public let issuedAt: IssuedAtClaim
-    public let expirationAt: ExpirationClaim
-    public let userID: String
     
+    /// Issued-at time. It must be in the past. The time is measured in seconds since the UNIX epoch.
+    public let issuedAt: IssuedAtClaim
+    
+    /// Expiration time. It must be in the future. The time is measured in seconds since the UNIX epoch.
+    public let expirationAt: ExpirationClaim
+    
+    /// The audience that this ID token is intended for. It must be your Firebase project ID, the unique identifier for your Firebase project, which can be found in the URL of that project's console.
+    public let audience: AudienceClaim
+    
+    /// Subject. It must be a non-empty string and must be the uid of the user or device.
+    public let subject: SubjectClaim
+    
+    /// Authentication time. It must be in the past. The time when the user authenticated.
+    public let authTime: Date?
+    
+    public let userID: String
     public let email: String?
     public let picture: String?
     public let name: String?
-    public let authTime: Date?
     public let isEmailVerified: Bool?
     public let phoneNumber: String?
-    
-    /// The audience that this ID token is intended for. It must be one of the OAuth 2.0 client IDs of your application.
-    public let audience: AudienceClaim
-    
-    /// An identifier for the user, unique among all Firebase accounts and never reused.
-    public let subject: SubjectClaim
     
     public func verify(using signer: JWTSigner) throws {
         guard self.issuer.value.contains("securetoken.google.com") else {
