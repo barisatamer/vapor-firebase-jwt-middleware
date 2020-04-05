b89624cef27d956a2b01229a65e3f2ded4d7afb9
Baris Atamer <brsatamer@gmail.com>
2020-04-05 18:09:59 +0200

    feat(jwksCache): add

---
+++ Sources/FirebaseJWTMiddleware/JWKSCache.swift
@@ -0,0 +1,6 @@
+import Vapor
+import JWT
+
+/// A thread-safe and atomic class for retrieving JSON Web Key Sets which honors the
+/// HTTP `Cache-Control`, `Expires` and `Etag` headers.
+public typealias JWKSCache = EndpointCache<JWKS>
