4080801e91694cfb53a0fe1c00c6bfc24a86696e
Baris Atamer <brsatamer@gmail.com>
2020-04-05 18:11:07 +0200

    feat(middleware): use request.firebaseJwt

--- Sources/FirebaseJWTMiddleware/FirebaseJWTMiddleware.swift
+++ Sources/FirebaseJWTMiddleware/FirebaseJWTMiddleware.swift
@@ -5,22 +5,7 @@ open class FirebaseJWTMiddleware: Middleware {
     
     public init() {}
     
-    public func respond(to request: Request, chainingTo next: Responder) throws -> EventLoopFuture<Response> {
-        if let token = request.http.headers[.authorization].first {
-            do {
-                try TokenVerifier.verify(token)
-                return try next.respond(to: request)
-            } catch let error as JWTError {
-                throw Abort(.unauthorized, reason: error.reason)
-            }
-        } else {
-            throw Abort(.unauthorized, reason: "No Access Token")
-        }
+    public func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
+        request.firebaseJwt.verify().transform(to: next.respond(to: request))
     }
 }
-
-enum JWTConfig {
-    static let header = JWTHeader(alg: "RS256", typ: "JWT")
-    static let expirationTime: TimeInterval = 50
-}
-
