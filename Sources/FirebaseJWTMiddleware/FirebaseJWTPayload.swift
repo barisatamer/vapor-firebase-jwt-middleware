18884d380933a4ffb4a880c56a5dded84d036d15
Baris Atamer <brsatamer@gmail.com>
2020-04-05 18:07:10 +0200

    feat(firebaseJwtPayload): add subject, audience, change verify

--- Sources/FirebaseJWTMiddleware/FirebaseJWTPayload.swift
+++ Sources/FirebaseJWTMiddleware/FirebaseJWTPayload.swift
@@ -21,6 +21,8 @@ public struct FirebaseJWTPayload: JWTPayload {
     
     enum CodingKeys: String, CodingKey {
         case issuer = "iss"
+        case subject = "sub"
+        case audience = "aud"
         case issuedAt = "iat"
         case expirationAt = "exp"
         case email = "email"
@@ -31,10 +33,21 @@ public struct FirebaseJWTPayload: JWTPayload {
         case isEmailVerified = "email_verified"
     }
     
+    /// The audience that this ID token is intended for. It must be one of the OAuth 2.0 client IDs of your application.
+    public let audience: AudienceClaim
+    
+    /// An identifier for the user, unique among all Firebase accounts and never reused.
+    public let subject: SubjectClaim
+    
     public func verify(using signer: JWTSigner) throws {
-        guard self.issuer.value == FirebaseJWTMiddlewareConfig.shared.issuer else {
-            throw JWTError.verificationFailed
+        guard self.issuer.value.contains("securetoken.google.com") else {
+            throw JWTError.claimVerificationFailure(name: "iss", reason: "Claim wasn't issued by Google")
         }
+        
+        guard self.subject.value.count <= 255 else {
+            throw JWTError.claimVerificationFailure(name: "sub", reason: "Subject claim beyond 255 ASCII characters long.")
+        }
+        
         try self.expirationAt.verifyNotExpired()
     }
 }
