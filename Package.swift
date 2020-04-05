6992c3162583ea00efc479e249cc250db5dc04a0
Baris Atamer <brsatamer@gmail.com>
2020-04-05 18:06:13 +0200

    chore(package): update

--- Package.swift
+++ Package.swift
@@ -1,19 +1,28 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FirebaseJWTMiddleware",
+    platforms: [
+       .macOS(.v10_15)
+    ],
     products: [
         .library(name: "FirebaseJWTMiddleware", targets: ["FirebaseJWTMiddleware"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0-rc"),
     ],
     targets: [
-        .target(name: "FirebaseJWTMiddleware", dependencies: ["Vapor", "JWT"]),
-        .testTarget(name: "FirebaseJWTMiddlewareTests", dependencies: ["FirebaseJWTMiddleware"]),
+        .target(name: "FirebaseJWTMiddleware", dependencies: [
+        .product(name: "Vapor", package: "vapor"),
+        .product(name: "JWT", package: "jwt")
+        ]),
+        .testTarget(name: "FirebaseJWTMiddlewareTests", dependencies: [
+            .target(name: "FirebaseJWTMiddleware"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ]),
     ]
 )
